f0985734a67bef55ae860ec84d970e3fd6655799
jenraii <jennaraiha7@gmail.com>
2023-09-05 16:21:27 +0300

    Spring Boot lisäys

---
+++ TicketGuru/src/test/java/kkvvsolutions/TicketGuru/TicketGuruApplicationTests.java
@@ -0,0 +1,13 @@
+package kkvvsolutions.TicketGuru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketGuruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
