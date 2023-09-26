faa0c684a33919abed834f803e737826224e1587
kkuismin <katariina.kuismin@gmail.com>
2023-09-26 15:55:13 +0300

    minor fix

--- TicketGuru/src/main/java/kkvvsolutions/TicketGuru/domain/TicketType.java
+++ TicketGuru/src/main/java/kkvvsolutions/TicketGuru/domain/TicketType.java
@@ -1,11 +1,14 @@
 package kkvvsolutions.TicketGuru.domain;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class TicketType {
