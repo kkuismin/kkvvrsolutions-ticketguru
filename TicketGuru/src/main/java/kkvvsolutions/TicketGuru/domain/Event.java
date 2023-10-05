443a93b7bd037a0c55e80935e5c363034d9e2479
jenraii <131862365+jenraii@users.noreply.github.com>
2023-10-05 17:41:59 +0300

    Update Event.java

--- TicketGuru/src/main/java/kkvvsolutions/TicketGuru/domain/Event.java
+++ TicketGuru/src/main/java/kkvvsolutions/TicketGuru/domain/Event.java
@@ -1,11 +1,10 @@
 package kkvvsolutions.TicketGuru.domain;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -13,11 +12,15 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.CascadeType;
+
 
 @Entity
 public class Event {
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "eventId", nullable = false, updatable = false)
 	private long eventId;
 
 	@ManyToOne
@@ -26,86 +29,113 @@ public class Event {
 
 	@JsonIgnore
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "event")
-	private List<TicketType> ticketTypes = new ArrayList<>();
+	private List<TicketType> ticketTypes;
 
 	@JsonIgnore
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "event")
-	private List<Ticket> tickets = new ArrayList<>();
+	private List<Ticket> tickets;
 
+	@Column(name = "name", nullable = false)
 	private String name;
+	
+	@Column(name = "date", nullable = false)
 	private String date;
+	
+	@Column(name = "time", nullable = false)
 	private String time;
+	
 
 	public Event() {
 		super();
 	}
+	
 
-	public Event(String name, String date, String time) {
+	public Event(long eventId, Venue venue, List<TicketType> ticketTypes, List<Ticket> tickets, String name,
+			String date, String time) {
 		super();
+		this.eventId = eventId;
+		this.venue = venue;
+		this.ticketTypes = ticketTypes;
+		this.tickets = tickets;
 		this.name = name;
 		this.date = date;
 		this.time = time;
 	}
 
+
 	public long getEventId() {
 		return eventId;
 	}
+
 
 	public void setEventId(long eventId) {
 		this.eventId = eventId;
 	}
 
-	public List<TicketType> getTicketTypes() {
-		return ticketTypes;
-	}
-
-	public void setTicketTypes(List<TicketType> ticketTypes) {
-		this.ticketTypes = ticketTypes;
-	}
-
-	public void setTickets(List<Ticket> tickets) {
-		this.tickets = tickets;
-	}
-
-	public List<Ticket> getTickets() {
-		return tickets;
-	}
 
 	public Venue getVenue() {
 		return venue;
 	}
 
+
 	public void setVenue(Venue venue) {
 		this.venue = venue;
+	}
+	
+	public List<TicketType> getTicketTypes() {
+		return ticketTypes;
+	}
+
+
+	public void setTicketTypes(List<TicketType> ticketTypes) {
+		this.ticketTypes = ticketTypes;
+	}
+
+
+	public List<Ticket> getTickets() {
+		return tickets;
+	}
+
+
+	public void setTickets(List<Ticket> tickets) {
+		this.tickets = tickets;
 	}
 
 	public String getName() {
 		return name;
 	}
 
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 
 	public String getDate() {
 		return date;
 	}
 
+
 	public void setDate(String date) {
 		this.date = date;
 	}
+
 
 	public String getTime() {
 		return time;
 	}
 
+
 	public void setTime(String time) {
 		this.time = time;
 	}
 
+
 	@Override
 	public String toString() {
-		return "Event [eventId=" + eventId + ", name=" + name + ", date=" + date + ", time=" + time + "]";
+		return "Event [eventId=" + eventId + ", venue=" + venue + ", ticketTypes=" + ticketTypes + ", tickets="
+				+ tickets + ", name=" + name + ", date=" + date + ", time=" + time + "]";
 	}
+	
 
 }
