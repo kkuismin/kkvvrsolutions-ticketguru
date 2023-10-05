04f18bce188a199721cf644de843438c678bf7c4
kkuismin <katariina.kuismin@gmail.com>
2023-10-05 15:48:55 +0300

    restticketcontrolleriin linkityksiä

--- TicketGuru/src/main/java/kkvvsolutions/TicketGuru/web/RestTicketController.java
+++ TicketGuru/src/main/java/kkvvsolutions/TicketGuru/web/RestTicketController.java
@@ -16,22 +16,37 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import kkvvsolutions.TicketGuru.domain.Event;
+import kkvvsolutions.TicketGuru.domain.SaleEvent;
 import kkvvsolutions.TicketGuru.domain.Ticket;
+import kkvvsolutions.TicketGuru.domain.TicketType;
+import kkvvsolutions.TicketGuru.domain.repository.EventRepository;
+import kkvvsolutions.TicketGuru.domain.repository.SaleEventRepository;
 import kkvvsolutions.TicketGuru.domain.repository.TicketRepository;
+import kkvvsolutions.TicketGuru.domain.repository.TicketTypeRepository;
 
 @RestController
 @RequestMapping("/api")
 public class RestTicketController {
 
 	@Autowired
-	private TicketRepository repository;
+	private TicketRepository trepository;
+	
+	@Autowired
+	private TicketTypeRepository ttrepository;
+	
+	@Autowired
+	private EventRepository erepository;
+	
+	@Autowired 
+	private SaleEventRepository srepository;
 
 	@GetMapping("/tickets")
 	public ResponseEntity<List<Ticket>> getAllTickets() {
 
 		try {
 			List<Ticket> tickets = new ArrayList<Ticket>();
-			repository.findAll().forEach(tickets::add);
+			trepository.findAll().forEach(tickets::add);
 
 			if (tickets.isEmpty()) {
 				return new ResponseEntity<>(HttpStatus.NO_CONTENT);
@@ -45,11 +60,62 @@ public class RestTicketController {
 	}
 
 	@GetMapping("/tickets/{id}")
-	public ResponseEntity<Ticket> getTicketById(@PathVariable("id") Long ticket_id) {
-		Optional<Ticket> ticketData = repository.findById(ticket_id);
+	public ResponseEntity<Ticket> getTicketById(@PathVariable("id") Long ticketId) {
+		Optional<Ticket> ticketData = trepository.findById(ticketId);
 
 		if (ticketData.isPresent()) {
 			return new ResponseEntity<>(ticketData.get(), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	@GetMapping("/tickets/{id}/tickettype")
+	public ResponseEntity<TicketType> getTicketType(@PathVariable("id") Long ticketId) {
+		Optional<Ticket> ticketData = trepository.findById(ticketId);
+		
+		if (ticketData.isPresent()) {
+			Ticket ticket = ticketData.get();
+			if (ticket.getTicketType() != null) {
+				TicketType ticketType = ticket.getTicketType();
+				return new ResponseEntity<>(ticketType, HttpStatus.OK);
+			} else {
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			}
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	@GetMapping("/tickets/{id}/event")
+	public ResponseEntity<Event> getEvent(@PathVariable("id") Long eventId) {
+		Optional<Ticket> ticketData = trepository.findById(eventId);
+		
+		if (ticketData.isPresent()) {
+			Ticket ticket = ticketData.get();
+			if (ticket.getEvent() != null) {
+				Event event = ticket.getEvent();
+				return new ResponseEntity<>(event, HttpStatus.OK);
+			} else {
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			}
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}	 
+	}
+	
+	@GetMapping("/tickets/{id}/sales")
+	public ResponseEntity<SaleEvent> getSaleEvent(@PathVariable("id") Long saleEventId) {
+		Optional<Ticket> ticketData = trepository.findById(saleEventId);
+		
+		if (ticketData.isPresent()) {
+			Ticket ticket = ticketData.get();
+			if (ticket.getSaleEvent() != null) {
+				SaleEvent saleEvent = ticket.getSaleEvent();
+				return new ResponseEntity<>(saleEvent, HttpStatus.OK);
+			} else {
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			}
 		} else {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
@@ -59,7 +125,7 @@ public class RestTicketController {
 	public ResponseEntity<Ticket> createTicket(@RequestBody Ticket ticket) {
 
 		try {
-			Ticket _ticket = repository.save(ticket);
+			Ticket _ticket = trepository.save(ticket);
 			return new ResponseEntity<>(_ticket, HttpStatus.CREATED);
 		} catch (Exception e) {
 			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
@@ -67,26 +133,26 @@ public class RestTicketController {
 	}
 
 	@PutMapping("/tickets/{id}")
-	public ResponseEntity<Ticket> updateTicket(@PathVariable("id") Long ticket_id, @RequestBody Ticket ticket) {
+	public ResponseEntity<Ticket> updateTicket(@PathVariable("id") Long ticketId, @RequestBody Ticket ticket) {
 
-		Optional<Ticket> ticketData = repository.findById(ticket_id);
+		Optional<Ticket> ticketData = trepository.findById(ticketId);
 
 		if (ticketData.isPresent()) {
 			Ticket _ticket = ticketData.get();
 			_ticket.setBarcode(ticket.getBarcode());
 			_ticket.setTicketType(ticket.getTicketType());
 
-			return new ResponseEntity<>(repository.save(_ticket), HttpStatus.OK);
+			return new ResponseEntity<>(trepository.save(_ticket), HttpStatus.OK);
 		} else {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
 	}
 
 	@DeleteMapping("/tickets/{id}")
-	public ResponseEntity<HttpStatus> deleteTicket(@PathVariable("id") Long ticket_id) {
+	public ResponseEntity<HttpStatus> deleteTicket(@PathVariable("id") Long ticketId) {
 
 		try {
-			repository.deleteById(ticket_id);
+			trepository.deleteById(ticketId);
 			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 		} catch (Exception e) {
 			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
