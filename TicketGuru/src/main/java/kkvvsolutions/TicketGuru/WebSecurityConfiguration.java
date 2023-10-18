8c9d968eb7edc8e6401be9e44a36bd98dbbfb39e
jenraii <131862365+jenraii@users.noreply.github.com>
2023-10-18 11:50:43 +0300

    Update WebSecurityConfiguration

--- TicketGuru/src/main/java/kkvvsolutions/TicketGuru/WebSecurityConfiguration.java
+++ TicketGuru/src/main/java/kkvvsolutions/TicketGuru/WebSecurityConfiguration.java
@@ -1,30 +1,33 @@
 package kkvvsolutions.TicketGuru;
 
-import kkvvsolutions.TicketGuru.service.DetailsService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import 
-org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-
 @Configuration
-public class WebSecurityConfiguration  {
+@EnableWebSecurity
+public class WebSecurityConfiguration {
 	
-	@Autowired
-	DetailsService detailsService;
-	
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-}
-@Bean
-public SecurityFilterChain filterChain(HttpSecurity http) throws Exception 
-{
-        http.authorizeHttpRequests((authz)->authz.anyRequest().permitAll());
-        return http.build();
-}
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(authorize -> {
+			authorize.anyRequest().authenticated();
+		}).formLogin(Customizer.withDefaults()) 
+				.csrf(AbstractHttpConfigurer::disable) 
+				.httpBasic(Customizer.withDefaults()) 
+				.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin())); 
+
+		return http.build();
+	}
 }
