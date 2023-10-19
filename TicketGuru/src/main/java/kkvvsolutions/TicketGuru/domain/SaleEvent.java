496342d43c7031eb4c18ea1090a8288d90174755
Joona Virolainen <91015110+Isanaiya@users.noreply.github.com>
2023-10-19 09:45:50 +0300

    Include Min() validation

--- TicketGuru/src/main/java/kkvvsolutions/TicketGuru/domain/SaleEvent.java
+++ TicketGuru/src/main/java/kkvvsolutions/TicketGuru/domain/SaleEvent.java
@@ -15,6 +15,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
 @Entity
@@ -38,6 +39,7 @@ public class SaleEvent {
 
 	@NotNull
 	@Column(name = "amount", nullable = false)
+	@Min(1)
 	private int amount;
 
 	public SaleEvent() {
