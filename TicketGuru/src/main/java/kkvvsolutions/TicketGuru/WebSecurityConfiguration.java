5dc4d2c24c6b2b31fa5676485fe1903d70ccc055
perttuvirkki <virkkiperttu@gmail.com>
2023-10-23 18:31:12 +0300

    additions to validation

--- TicketGuru/src/main/java/kkvvsolutions/TicketGuru/WebSecurityConfiguration.java
+++ TicketGuru/src/main/java/kkvvsolutions/TicketGuru/WebSecurityConfiguration.java
@@ -1,40 +1,57 @@
 package kkvvsolutions.TicketGuru;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import kkvvsolutions.TicketGuru.web.UserDetailServiceImpl;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
-@EnableMethodSecurity(securedEnabled = true)
 public class WebSecurityConfiguration {
-	
-	@Autowired
-	private UserDetailServiceImpl userDetailsService;
 
-	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests(authorize -> {
-			authorize.anyRequest().authenticated();
-		}).formLogin(Customizer.withDefaults()) 
-				.csrf(AbstractHttpConfigurer::disable) 
-				.httpBasic(Customizer.withDefaults()) 
-				.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin())); 
+        @Bean
+        public static PasswordEncoder passwordEncoder() {
+                return new BCryptPasswordEncoder();
+        }
 
-		return http.build();
-	}
-	
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
-	}
-	
+        @Bean
+        SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+                http.csrf(Customizer.withDefaults())
+                                .authorizeHttpRequests((authorize) -> {
+                                        authorize.anyRequest().authenticated();
+                                })
+                                .httpBasic(Customizer.withDefaults())
+                                .csrf((csrf) -> csrf
+                                                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                                                .csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler()))
+                                .formLogin(Customizer.withDefaults())
+                                .httpBasic(Customizer.withDefaults());
+
+                return http.build();
+        }
+
+        @Bean
+        public WebSecurityCustomizer webSecurityCustomizer() {
+                return (web) -> web.ignoring().requestMatchers(new AntPathRequestMatcher("/h2-console/**"));
+        }
+
+        @Bean
+        public UserDetailsService userDetailsService() {
+
+                UserDetails admin = User.builder().username("admin").password(passwordEncoder().encode("admin"))
+                                .roles("ADMIN")
+                                .build();
+
+                return new InMemoryUserDetailsManager(admin);
+        }
 }
