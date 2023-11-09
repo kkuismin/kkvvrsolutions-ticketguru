db517cbef90904cf5a56804e0901065376c1bad0
jenraii <131862365+jenraii@users.noreply.github.com>
2023-11-09 10:28:22 +0200

    Update TicketRepository

--- TicketGuru/src/main/java/kkvvsolutions/TicketGuru/domain/repository/TicketRepository.java
+++ TicketGuru/src/main/java/kkvvsolutions/TicketGuru/domain/repository/TicketRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import kkvvsolutions.TicketGuru.domain.Ticket;
 
 public interface TicketRepository extends CrudRepository<Ticket, Long> {
-
+      Optional<Ticket> findByBarcode (String barcode);
 }
