f79fddfaa5cb93e4cd5ea5eb56b69abb496f5567
jenraii <131862365+jenraii@users.noreply.github.com>
2023-11-20 18:18:37 +0200

    TicketTypeRepository lisäys

--- TicketGuru/src/main/java/kkvvsolutions/TicketGuru/domain/repository/TicketTypeRepository.java
+++ TicketGuru/src/main/java/kkvvsolutions/TicketGuru/domain/repository/TicketTypeRepository.java
@@ -1,9 +1,11 @@
 package kkvvsolutions.TicketGuru.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import kkvvsolutions.TicketGuru.domain.TicketType;
 
 public interface TicketTypeRepository extends CrudRepository<TicketType, Long> {
-
+      List<TicketType> findByTicketName(String ticketName);
 }
