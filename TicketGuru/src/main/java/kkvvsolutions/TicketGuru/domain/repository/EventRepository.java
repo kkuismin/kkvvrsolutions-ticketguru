2752e58fd54abb5d84cb6e17acabdbe79864847e
jenraii <131862365+jenraii@users.noreply.github.com>
2023-11-20 18:15:27 +0200

    EventRepository lisäys

--- TicketGuru/src/main/java/kkvvsolutions/TicketGuru/domain/repository/EventRepository.java
+++ TicketGuru/src/main/java/kkvvsolutions/TicketGuru/domain/repository/EventRepository.java
@@ -1,9 +1,11 @@
 package kkvvsolutions.TicketGuru.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import kkvvsolutions.TicketGuru.domain.Event;
 
 public interface EventRepository extends CrudRepository<Event, Long> {
-
+  List<Event> findByVenueName(String string);
 }
